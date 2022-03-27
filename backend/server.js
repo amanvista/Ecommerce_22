25bbc83a54b6ff98e4e22ab6cab733e5c5720fbe
Aman <amanvista@gmail.com>
2022-03-27 15:14:41 +0530

    1.Test Route Working Fine

---
+++ backend/server.js
@@ -0,0 +1,9 @@
+const app = require("./app")
+
+const dotenv = require("dotenv")
+
+//Config
+dotenv.config({path: "backend/config/config.env"})
+app.listen(process.env.PORT, ()=>{
+    console.log(`Server is working on https://localhost:${process.env.PORT}`)
+})
